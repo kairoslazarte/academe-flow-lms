96aba64ecbd68ded3be0373bd79b96a46f07be9a
kairoslazarte <thelazarte@gmail.com>
2025-02-09 21:19:03 +0800

    fix import of daisy ui plugin in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -8,7 +10,7 @@ export default {
 		},
 	},
 	// eslint-disable-next-line no-undef
-	plugins: [require("daisyui")],
+	plugins: [daisyui],
 	daisyui: {
 		darkTheme: "light",
 	},
